09472d94b400558a2451dc484a4ac6ad11e2d9cb
Zilas <pyeroevo@gmail.com>
2019-06-30 14:30:05 -0400

    Rota para os formularios de contato e brief

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function (req, res, next) {
   res.send('index');
 });
 
-router.post("/api", (req, res) => {
+router.post("/api/inscricao", (req, res) =>{
   const content = req.body;
   const message = "Nome: "+content.nome+"\n"+
                   "Email: "+content.email+"\n"+
@@ -26,5 +26,48 @@ router.post("/api", (req, res) => {
 
   res.send();
 });
+
+router.post("api/brief", (req, res) =>{
+  const content = req.body;
+  const message = "Nome: "+content.nome+"\n"+
+                  "Email: "+content.email+"\n"+
+                  "Telefone: "+content.telefone+"\n"+
+                  "Curso: "+content.curso+"\n"+
+                  "Data de nascimento: "+content.dataNasc+"\n"+
+                  "Conhecimentos: "+content.conhecimentos+"\n\n"+
+                  "Mensagem\n"+
+                    "\tProblemas: "+content.problema+"\n"+
+                    "\tBeneficios: "+content.beneficios+"\n"+
+                    "\tPublico alvo: "+content.publicoAlvo+"\n"+
+                    "\tProduto de interesse: "+content.produtoInteresse+"\n"+
+                    "\tExpectativa: "+content.expectativa+"\n";
+  
+  fetch(/* curl */, {
+    body: "{\"text\":\"" + message + "\"}",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    method: "post"
+  });
+
+  res.send();
+})
+
+router.post("api/contato", (req, res) =>{
+  const content = req.body;
+  const message = "Nome: "+content.nome+"\n"+
+                  "Email: "+content.email+"\n"+
+                  "Assunto: "+content.subject;
+
+  fetch(/* curl */, {
+    body: "{\"text\":\"" + message + "\"}",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    method: "post"
+  });
+
+  res.send();
+})
 
 module.exports = router;
